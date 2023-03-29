526f77dac33c85189151bba458f52e95e3669ae6
rytpiQWE <rytpiss@gmail.com>
2023-03-29 15:22:13 +0700

    change register

--- app/desk/admin.py
+++ app/desk/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from desk.models import Category, Desk
+from desk.models import Category, Desk, DeskImage
+
+
+class DeskImageAdmin(admin.StackedInline):
+    model = DeskImage
+
+
+@admin.register(Desk)
+class DeskAdminPanel(ModelAdmin):
+    inlines = [DeskImageAdmin]
+
+    class Meta:
+        model = Desk
 
 
 @admin.register(Category)
-@admin.register(Desk)
-class AdminPanel(ModelAdmin):
+class CategoryAdminPanel(ModelAdmin):
+    pass
+
+
+@admin.register(DeskImage)
+class ImageAdminPanel(ModelAdmin):
     pass
