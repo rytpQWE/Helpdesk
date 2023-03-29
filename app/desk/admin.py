91df56341d43e652b31d36521a286e324347d0f7
rytpiQWE <rytpiss@gmail.com>
2023-03-29 12:45:30 +0700

    register models

--- app/desk/admin.py
+++ app/desk/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from desk.models import Category, Desk
+
+
+@admin.register(Category)
+@admin.register(Desk)
+class AdminPanel:
+    pass
