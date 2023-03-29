d5a340fed65f6f355a39b5bc64cb12b75ae3b216
rytpiQWE <rytpiss@gmail.com>
2023-03-29 13:40:52 +0700

    added images

--- app/desk/models.py
+++ app/desk/models.py
@@ -20,5 +20,11 @@ class Desk(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     comment = models.TextField(max_length=2000)
+    image = models.FileField()
     status = models.CharField(choices=STATUS_TYPE, max_length=30, default='Accepted')
+
+
+class DeskImage(models.Model):
+    desk = models.ForeignKey(Desk, on_delete=models.CASCADE)
+    images = models.ImageField(upload_to='images/')
 
