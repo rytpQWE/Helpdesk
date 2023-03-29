3c95a0fa424e29eafff1c1b1949cbfe7b94981b0
rytpiQWE <rytpiss@gmail.com>
2023-03-29 11:45:22 +0700

    added Category + Desk models

--- app/desk/models.py
+++ app/desk/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+
+
+class Desk(models.Model):
+    STATUS_TYPE = (
+        ('accepted', 'Accepted'),
+        ('in_progress', 'In progress'),
+        ('completed', 'Completed'),
+    )
+    User = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=2000)
+    status = models.CharField(choices=STATUS_TYPE, max_length=30, default='Accepted')
+
