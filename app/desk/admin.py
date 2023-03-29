54ad557af311329975b62df93492384e55aa0122
rytpiQWE <rytpiss@gmail.com>
2023-03-29 14:02:42 +0700

    added relation ModelAdmin

--- app/desk/admin.py
+++ app/desk/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from desk.models import Category, Desk
 
 
 @admin.register(Category)
 @admin.register(Desk)
-class AdminPanel:
+class AdminPanel(ModelAdmin):
     pass
