bd077ccb4038b91d38e6027c9c45e8a40b567177
rytpiQWE <rytpiss@gmail.com>
2023-04-02 21:44:55 +0700

    added DeskViewSet(main page)

--- app/desk/views.py
+++ app/desk/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from desk.models import Desk
+from desk.serializers import DeskCreateSerializer
+
+
+class DeskViewSet(viewsets.GenericViewSet,
+                  mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  ):
+    queryset = Desk.objects.all()
+    serializer_class = DeskCreateSerializer
+
+    def perform_create(self, serializer):
+        """Create and save current user in form(desk)"""
+        serializer.save(user=self.request.user)
+
+
