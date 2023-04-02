cdbb56d5f886e4757d20a0badb730d8e6e57de23
rytpiQWE <rytpiss@gmail.com>
2023-04-02 21:44:29 +0700

    added main page

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+
+from desk import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+router = routers.DefaultRouter()
+router.register('api/desk', views.DeskViewSet)
+
+urlpatterns += router.urls
