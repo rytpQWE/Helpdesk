d206b2d652f52b2f78b064811d2e5871ccab122f
rytpiQWE <rytpiss@gmail.com>
2023-04-03 10:28:30 +0700

    added deskSerializer

--- app/desk/serializers.py
+++ app/desk/serializers.py
@@ -1,10 +1,37 @@
 from rest_framework import serializers
 
-from desk.models import Desk
+from desk.models import Desk, DeskImage
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeskImage
+        fields = ['images']
 
 
 class DeskCreateSerializer(serializers.ModelSerializer):
+    images_set = ImagesSerializer(many=True, source='img', read_only=True)
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(max_length=100000, allow_empty_file=True, use_url=False),
+        write_only=True,
+        default=None,
+        required=False,
+    )
+
     class Meta:
         model = Desk
-        fields = ['id', 'User', 'title', 'created_at', 'category', 'comment', 'image', 'status']
+        fields = ['id', 'User', 'title', 'created_at', 'category', 'comment', 'status', 'images_set', 'uploaded_images']
         read_only_fields = ['status']
+        extra_kwargs = {'uploaded_images': {"required": False, "allow_null": True}}
+
+    # Upload files(images) only in postman
+    def create(self, validated_data):
+        """Method override for save multiply images"""
+        uploaded_images = validated_data.pop("uploaded_images")
+        desk = Desk.objects.create(**validated_data)
+        if uploaded_images == None:
+            pass
+        else:
+            for image in uploaded_images:
+                newdesk_image = DeskImage.objects.create(desk=desk, images=image)
+        return desk
