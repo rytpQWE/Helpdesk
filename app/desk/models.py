5a533145d859cdb795a15e6b2cec10585a323543
rytpiQWE <rytpiss@gmail.com>
2023-04-09 19:18:44 +0700

    Employee model + auto create employee status

--- app/desk/models.py
+++ app/desk/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Category(models.Model):
@@ -48,3 +50,16 @@ class DeskImage(models.Model):
     def __str__(self):
         return f'Image id: {self.pk}'
 
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='employee')
+    is_employee = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Employee: {self.user}'
+
+
+@receiver(post_save, sender=User)
+def employee_create(sender, instance=None, created=False, **kwargs):
+    if created:
+        Employee.objects.create(user=instance)
