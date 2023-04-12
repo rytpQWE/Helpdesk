2ffc1a54c48f25daa90c02301b16edf8f8d55830
rytpiQWE <rytpiss@gmail.com>
2023-04-12 13:27:54 +0700

    djoser settings: auth email confirmation

--- app/app/settings.py
+++ app/app/settings.py
@@ -150,3 +150,11 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rytpiss@gmail.com'
 EMAIL_HOST_PASSWORD = 'ehckadakryxetvuw'
 EMAIL_PORT = 587
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'SERIALIZERS': {},
+}
