4ec857b91710da3a758e383bf28d81ab1243e593
rytpiQWE <rytpiss@gmail.com>
2023-04-12 13:30:07 +0700

    djoser settings: auth email confirmation

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
@@ -23,6 +23,8 @@ from desk import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 router = routers.DefaultRouter()
