086db8d2aba34cfe293f713c5457452c7348da3d
rytpiQWE <rytpiss@gmail.com>
2023-04-17 20:05:06 +0700

    rename task

--- app/desk/tasks.py
+++ app/desk/tasks.py
@@ -5,11 +5,11 @@ from desk.models import Desk
 
 
 @shared_task
-def send_mail(desk_id):
+def send_mail_for_user(desk_id):
     desk = Desk.objects.get(id=desk_id)
     email = EmailMessage(
         'HelpDesk',
-        f'Your application: {desk.title} has been completed',
+        f'Your application: {desk.title} has been completed.',
         to=[str(desk.User.email)]
     )
     email.send()
