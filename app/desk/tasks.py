6bc25906ceb259be56d3da272a18d9abeec4abe5
rytpiQWE <rytpiss@gmail.com>
2023-04-17 20:04:45 +0700

    added send mail for employee

--- app/desk/tasks.py
+++ app/desk/tasks.py
@@ -13,3 +13,25 @@ def send_mail(desk_id):
         to=[str(desk.User.email)]
     )
     email.send()
+
+
+@shared_task
+def send_employee_mail(desk_id):
+    # TODO: Вынести за таску !!!!!!!!
+    # employees = Employee.objects.filter(is_employee=True).values_list('pk', flat=True)
+    # employees_emails = [User.objects.get(id=x).email for x in employees]
+    for mail in employees_emails:
+        send_mail_for_employee.delay(desk_id, mail)
+
+
+@shared_task
+def send_mail_for_employee(desk_id, mail):
+    desk = Desk.objects.get(id=desk_id)
+    email = EmailMessage(
+        'NEW TICKET HelpDesk',
+        f'Ticket: {desk.title}',
+        to=[str(mail)]
+    )
+    email.send()
+
+
