865193a645973f83f58c117ae646136c44489eb0
rytpiQWE <rytpiss@gmail.com>
2023-04-21 13:49:44 +0700

    delete comment

--- app/desk/tasks.py
+++ app/desk/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
+from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 
-from desk.models import Desk
+from desk.models import Desk, Employee
 
 
 @shared_task
@@ -17,9 +18,8 @@ def send_mail_for_user(desk_id):
 
 @shared_task
 def send_employee_mail(desk_id):
-    # TODO: Вынести за таску !!!!!!!!
-    # employees = Employee.objects.filter(is_employee=True).values_list('pk', flat=True)
-    # employees_emails = [User.objects.get(id=x).email for x in employees]
+    employees = Employee.objects.filter(is_employee=True).values_list('pk', flat=True)
+    employees_emails = [User.objects.get(id=x).email for x in employees]
     for mail in employees_emails:
         send_mail_for_employee.delay(desk_id, mail)
 
