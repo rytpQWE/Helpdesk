fcc8c58215120a8cd9cb8e1f64b8adab80cd1f87
rytpiQWE <rytpiss@gmail.com>
2023-04-23 20:41:17 +0700

    change settings Celery

--- app/celery_app.py
+++ app/celery_app.py
@@ -7,9 +7,9 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-app = Celery('app')
+app = Celery('app', backend='redis://redis:6379/0', broker='redis://redis:6379/0')
 app.config_from_object('django.conf:settings')
-app.conf.broker_url = settings.CELERY_BROKER_URL
+# app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
 
